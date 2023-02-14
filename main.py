907f455c02664b93a29c5a9bed959d7f8bad7c48
vardaansinha <vardsin28@gmail.com>
2023-02-14 14:24:45 -0800

    removed nonsense

--- main.py
+++ main.py
@@ -7,38 +7,16 @@ from flask import render_template  # import render_template from "public" flask 
 from __init__ import app  # Definitions initialization
 from model.nflteam import initNFLTeams
 from model.scores import initCool
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-#from model.facts import initFactDay
-=======
-# from model.facts import initFactDay
->>>>>>> 6fb1fff4138dee83618a58c35b8eff43e5b76e36
->>>>>>> faa773463bc885869ed5cddbf94570c047516705
+
 
 # setup APIs
 from api.nflteam import nflteam_api # Blueprint import api definition
 from api.score import score_api # Blueprint import api definition
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-#from api.fact import fact_api
-=======
-# from api.fact import fact_api
->>>>>>> 6fb1fff4138dee83618a58c35b8eff43e5b76e36
->>>>>>> faa773463bc885869ed5cddbf94570c047516705
+
 
 # register URIs
 app.register_blueprint(nflteam_api) # register app pages
 app.register_blueprint(score_api)
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-#app.register_blueprint(fact_api)
-=======
-# app.register_blueprint(fact_api)
->>>>>>> 6fb1fff4138dee83618a58c35b8eff43e5b76e36
->>>>>>> faa773463bc885869ed5cddbf94570c047516705
 
 
 @app.errorhandler(404)  # catch for URL not found
